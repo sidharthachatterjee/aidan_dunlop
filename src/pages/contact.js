606478c7bfd6ca68e30a217b0fbfacd11d31adc1
Aidan Dunlop <aidandunlop@gmail.com>
2018-10-10 11:02:12 +0100

    started playing around with colour scheme

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/navigation/defaultLayout';
 
 const Contact = () => (
   <Layout>
-    <h1>Please contact me at:</h1>
-    <Link to="/">Go back to the homepage</Link>
+    {'You can'}
+    <ul>
+      <li>
+        <a href="mailto:aidandunlop@gmail.com">email me</a>
+      </li>
+      {/* put contact form in here */}
+      <li>
+        {'Follow me on '}
+        <a href="https://github.com/aidandunlop">Github</a>
+      </li>
+      <li>
+        {'Follow me on '}
+        <a href="https://twitter.com/aidandunlop">Twitter</a>
+      </li>
+      <li>
+        {'Connect with me on '}
+        <a href="https://linkedin.com/aidandunlop">LinkedIn</a>
+      </li>
+    </ul>
+
   </Layout>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,20 @@ import Layout from '../components/navigation/defaultLayout';
 const IndexPage = () => (
   <Layout>
     <h1>Who?</h1>
-    <p>I&apos;m a software developer, currently at NOWTV. </p>
-    <p>Interested in Machine Learning, Computer Vision, IoT and all things tech. </p>
-    <p>Big fan of disco. </p>
+    <p>
+      I&apos;m a software developer, currently at
+      <a href="https://nowtv.com"> NOW TV</a>
+      .
+    </p>
+    <p>
+      I have a 1st class Computer Science degree from the
+      <a href="https://www.manchester.ac.uk/"> University of Manchester</a>
+      .
+    </p>
+    <p>
+      I&apos;m interested in Machine Learning, Computer Vision and IoT.
+    </p>
+    <p>Big fan of disco, festivals and live music.</p>
     {/* add spotify playlist link */}
   </Layout>
 );
