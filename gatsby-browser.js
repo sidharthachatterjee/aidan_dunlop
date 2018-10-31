825b3c16f0337e58c7261dabfb115d60e79a8335
Aidan Dunlop <aidandunlop@gmail.com>
2018-10-31 07:45:26 +0000

    remove console.log

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 const React = require('react');
 const Root = require('./src/components/root').default;
 
-console.log(Root);
 exports.wrapRootElement = ({ element }) => (//eslint-disable-line
   <Root element={element} />
 );
