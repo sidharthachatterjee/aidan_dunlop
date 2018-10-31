3eebd60748b09574f90d3e785c7d0a78bbb8c1c9
Aidan Dunlop <aidandunlop@gmail.com>
2018-10-31 08:23:45 +0000

    use inline styles for bgd

--- src/components/background/background.js
+++ src/components/background/background.js
@@ -2,8 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './background.scss';
 
+const styles = {
+  backgroundSize: 'cover',
+  backgroundAttachment: 'fixed',
+  position: 'fixed',
+  left: '0',
+  height: '100%',
+  right: '0',
+  zIndex: -'1',
+  filter: 'brightness(0.5)',
+};
+
 const Background = ({ source }) => (
-  <div className="background" style={{ backgroundImage: `url(${source})` }} />
+  <div style={{ ...styles, ...{ backgroundImage: `url(${source})` } }} />
 );
 
 export default Background;
